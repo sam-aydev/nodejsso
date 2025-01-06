4273adc39526cc6789fefd7e273a00422c5b5482
sam-aydev <125776151+sam-aydev@users.noreply.github.com>
2025-01-06 14:57:29 +0000

    update

--- server.js
+++ server.js
@@ -7,13 +7,38 @@ const adminRoutes = require("./routes/admin-routes")
 const imageRoutes = require("./routes/image-routes")
 const productRoutes = require("./routes/product-routes")
 const bookRoutes = require("./routes/book-routes")
+const http = require("http")
+const socketio = require("socket.io")
 const app = express()
 
 
+
+const server = http.createServer(app)
+const io = socketio(server)
+app.use(express.static("public"))
+
+const users = new Set()
+io.on("connection", (socket)=> {
+    console.log("User is now connected");
+
+
+    // handle when they join chat
+    socket.on("join", (userName)=>{
+        user.add(userName)
+
+        // broadcast to all users
+        io.emit("userJoined", userName)
+
+        // Send updated userlist to clients
+        io.emit("usersList", Array.from(users))
+    })
+
+
+})
 app.get("/", (req, res)=>{
     res.send("hello")
 })
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3001
 
 connectToDb()
 app.use(express.json())
